4322686594c554c9571b9a330e994ccf13e23a7b
aaronsleepy <aaron@kmong.com>
2022-11-19 17:11:25 +0900

    feat: ChatList CollectionViewCell 추가

---
+++ ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by Aaron on 2022/11/19.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+}
